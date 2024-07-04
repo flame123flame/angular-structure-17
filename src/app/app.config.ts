e62100a8382004b413167767fbff5d1055c6a87e
flame123flame <flame.tana@gmail.com>
2024-07-04 17:07:47 +0700

    add

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,14 +6,15 @@ import { provideNzIcons } from './icons-provider';
 import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { apiInterceptor } from './core/interceptors/api.interceptor';
 import { errorInterceptor } from './core/interceptors/error.interceptor';
+import { SpinnerInterceptorService } from './core/services/spinner-interceptor.service';
+
 
 registerLocaleData(en);
-
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -24,17 +25,15 @@ export const appConfig: ApplicationConfig = {
     ),
     provideNzIcons(),
     provideNzI18n(en_US),
-    importProvidersFrom(FormsModule),
+    importProvidersFrom(FormsModule, ReactiveFormsModule),
     provideAnimationsAsync(),
     provideHttpClient(
       withInterceptors([
         apiInterceptor,
         errorInterceptor
-        // { provide: HTTP_INTERCEPTORS, useClass: SpinnerInterceptorService, multi: true },
-        // { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
       ])
     ),
+    { provide: HTTP_INTERCEPTORS, useClass: SpinnerInterceptorService, multi: true },
+
   ]
 };
-
-
--- src/app/modules/authentication/pages/sign-in/sign-in.component.ts
+++ src/app/modules/authentication/pages/sign-in/sign-in.component.ts
@@ -1,36 +1,54 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthMockupService } from '../../../../core/mockup-api/auth-mockup.service';
+import { NgZorroAntdModule } from '../../../../shared/ng-zorro-antd.module';
 import { SharedModule } from '../../../../shared/shared.module';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [SharedModule],
+  imports: [SharedModule, NgZorroAntdModule],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss'
 })
 export class SignInComponent {
-  username: string = '';
-  password: string = '';
+  loginForm: FormGroup;
   errorMessage: string = '';
 
-  constructor(private authService: AuthMockupService,
-    private router: Router,) { }
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthMockupService,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  ngOnInit(): void { }
 
   login(): void {
-    this.authService.login(this.username, this.password).subscribe(response => {
-      if (response) {
-        this.router.navigate(['/feature/dashboard']);
-        console.log('Login successful', response);
-        // Handle successful login here
-      } else {
-        this.errorMessage = 'Invalid username or password';
-      }
-    }, error => {
-      console.error('Login failed', error);
-      this.errorMessage = 'Login failed. Please try again later.';
-    });
+    if (this.loginForm.valid) {
+      const { username, password } = this.loginForm.value;
+      this.authService.login(username, password).subscribe(
+        response => {
+          if (response) {
+            this.router.navigate(['/feature/dashboard']);
+            console.log('Login successful', response);
+          } else {
+            this.errorMessage = 'Invalid username or password';
+          }
+        },
+        error => {
+          console.error('Login failed', error);
+          this.errorMessage = 'Login failed. Please try again later.';
+        }
+      );
+    } else {
+      this.errorMessage = 'Please fill out the form correctly.';
+    }
   }
 
 }
